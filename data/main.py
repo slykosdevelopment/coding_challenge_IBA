1d312988456f7e75a3295c7322b6b840c56b1560
Slykos development <slykos.dev@gmail.com>
2024-03-23 16:35:54 +0100

    created read csv functions

--- data/main.py
+++ data/main.py
@@ -2,7 +2,31 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+def readFile(filename):
+    f=open(filename)
+    content=f.read()
+    return [line for line in content.split('\n')]
+
+def getSignalsList():
+    res=readFile("signals.csv")
+    return res
+
+def getKeywordsList():
+    res=readFile("keywords.csv")
+    return res
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+
+
+    return {"debug" : getKeywordsList()}
+
+    """
+    signals_list=["testa","testb"]
+    specific_signal="testc"
+    
+    return {
+        "List of signals": signals_list,
+        "Specific signal given its node id ":specific_signal
+    }
+    """

53233133e42ce430599ac1e69a7fe915ab911187
Slykos development <slykos.dev@gmail.com>
2024-03-23 17:17:44 +0100

    started processing signals

--- data/main.py
+++ data/main.py
@@ -7,20 +7,36 @@ def readCSV(filename):
     content=f.read()
     return [line.split(",") for line in content.split('\n')][1:-1]
 
-def getSignalsList():
-    res=readCSV("signals.csv")
-    return res
+def getRawSignalsList():
+    return readCSV("signals.csv")
 
-def getKeywordsList():
-    res=readCSV("keywords.csv")
-    return res
+def getRawKeywordsList():
+    return readCSV("keywords.csv")
+
+def getProcessedSignalsList():
+    keywords_list=getRawKeywordsList()
+    signal_list=getRawSignalsList()
+
+    for record in signal_list:
+        this_record_keywords_id=record.pop()
+        this_record_keywords=[]
+        for car in this_record_keywords_id:
+            if car.isdigit():
+                this_record_keywords.append(findKeyWord(keywords_list, car))
+        record.append(this_record_keywords)
+    return signal_list
+
+def findKeyWord(keywords_list, keyword_id):
+    #return keywords_list[int(keyword_id)-1]
+    return int(keyword_id)
+
 
 @app.get("/")
 async def root():
 
-
-    return {"debug" : getKeywordsList()}
-    #return {"debug" : getSignalsList()}
+    return {"debug" : getProcessedSignalsList()}
+    #return {"debug" : getRawKeywordsList()}
+    #return {"debug" : getRawSignalsList()}
 
     """
     signals_list=["testa","testb"]

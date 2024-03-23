e237ed4669f7ee57ee61d789a8aba41e74b121fa
Slykos development <slykos.dev@gmail.com>
2024-03-23 17:53:24 +0100

    improved code

--- data/main.py
+++ data/main.py
@@ -16,18 +16,30 @@ def getRawKeywordsList():
 def getProcessedSignalsList():
     keywords_list=getRawKeywordsList()
     signal_list=getRawSignalsList()
+    processed_signals_list=[]
 
     for record in signal_list:
-        if len(record)==6:
-            this_record_keywords_id=record.pop()
-            this_record_keywords=[]
-            for car in this_record_keywords_id:
-                if car.isdigit():
-                    this_record_keywords.append(findKeyWord(keywords_list, car))
-            record.append(this_record_keywords)
-        else:
-            del record
-    return signal_list
+        new_record=processRecord(record, keywords_list)
+        if new_record!=None:
+            processed_signals_list.append(new_record)
+
+    return processed_signals_list
+
+
+def processRecord(record, keywords_list):
+    new_record=[]
+
+    if len(record)==6:
+        new_record=record[:5][::]
+        this_record_keywords_id=record[-1]
+        this_record_keywords=[]
+        for car in this_record_keywords_id:
+            if car.isdigit():
+                this_record_keywords.append(findKeyWord(keywords_list, car))
+        new_record.append(this_record_keywords)
+    else:
+        new_record=None
+    return new_record
 
 def findKeyWord(keywords_list, keyword_id):
     return keywords_list[int(keyword_id)-1][1]

fe0e1ab76271264241cd035ab3f23f295c7cddc5
Slykos development <slykos.dev@gmail.com>
2024-03-28 01:45:41 +0100

    split into two options for docker

---
+++ data/script_controller.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+if [ "$1" == "create_database" ]; then
+    python3 createDatabase.py
+
+elif [ "$1" == "launch_program" ]; then
+    uvicorn main:app --host 0.0.0.0 --port 8000
+
+else
+    echo "specify aunch_program)"
+fi

adf16b4acb1377f65e9912f3c879f2e8c578c711
Slykos development <slykos.dev@gmail.com>
2024-03-28 02:48:55 +0100

    renamed variables

--- data/misc.py
+++ data/misc.py
@@ -20,41 +20,35 @@ def getProcessedSignalsList():
     processed_signals_list=[]
 
     for record in signal_list:
-        new_record=processRecord(record, keywords_list)
-        if new_record!=None:
-            processed_signals_list.append(new_record)
+        if len(record)==6:
+            processed_signal=processSignal(record, keywords_list)
+            processed_signals_list.append(processed_signal)
 
     return processed_signals_list
 
+def processSignal(record, keywords_list):
 
-def processRecord(record, keywords_list):
+    new_record=[None for i in range (6)]
 
-    if len(record)==6:
-        new_record=[None for i in range (6)]
+    for i in range (6):
 
-        for i in range (6):
+        if i in [1,2,4]: #if data is int
 
-            if i in [1,2,4]: #if data is int
+            if record[i].isdigit():
+                new_record[i]=int(record[i])
+            else:
+                new_record[i]=-1
 
-                if record[i].isdigit():
-                    new_record[i]=int(record[i])
-                else:
-                    new_record[i]=-1
+        elif i in [0,3]: #if data is str
+            new_record[i]=str(record[i])
 
-            elif i in [0,3]: #if data is str
-                new_record[i]=str(record[i])
+        else: #if on the keywords list
+            keywords_lst=[]
+            
+            for car in record[5].strip('[]').split(";"):
+                if len(car)>0:
+                    keywords_lst.append(findKeyWord(keywords_list, car))
 
-            else: #if on the keywords list
-                keywords_lst=[]
-                
-                for car in record[5].strip('[]').split(";"):
-                    if len(car)>0:
-                        keywords_lst.append(findKeyWord(keywords_list, car))
+            new_record[5]=", ".join(keywords_lst)
 
-                new_record[5]=", ".join(keywords_lst)
-
-        return new_record
-
-    else:
-        return None
-
+    return new_record
